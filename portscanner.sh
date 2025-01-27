25715303b9b1ba9840605f2cc5e55653853c7c3b
AndyAP7 <97900508+AndyAP7@users.noreply.github.com>
2025-01-26 21:47:56 -0500

    Create portscanner.sh

---
+++ portscanner.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+hostfile=$1
+portfile=$2
+
+if [ ! -f "Shostfile" ]; then
+        echo "File not found" 
+        exit 1
+fi
+echo "host, portostatus"
+
+for host in (cat $hostfile) do
+    for port in $(cat $portfile); do 
+            timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null && status="open" || status="closed
+            echo "$host, $port, $status"
+      done
+done
