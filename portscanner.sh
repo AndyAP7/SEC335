84e81f6221bb05447c863b45a26c1f19037b22c2
AndyAP7 <97900508+AndyAP7@users.noreply.github.com>
2025-01-28 14:50:12 -0500

    Update portscanner.sh

--- portscanner.sh
+++ portscanner.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-hostfile=$1
-portfile=$2
+hfile=$1
+pfile=$2
 
-if [ ! -f "$hostfile" ]; then
+if [ ! -f "$hfile" ]; then
         echo "File not found" 
         exit 1
 fi
-echo "host, port,status"
+echo "host, port, status"
 
-for host in (cat $hostfile) do
-    for port in $(cat $portfile); do 
-            timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null && status="open" || status="closed
+for host in (cat $hfile) do
+    for port in $(cat $pfile); do 
+            timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null 
+            && status="open port" || status="closed port"
             echo "$host, $port, $status"
       done
 done
