69ea97f1b27f4a178a4478432657b81e7678aa76
Andy <andy.phuong@mymail.champlain.edu>
2025-01-28 14:32:50 -0500

    dns res file

---
+++ dnsres.ps1
@@ -0,0 +1,16 @@
+﻿param ($networkprefix, $dnsserver)
+$start = 1
+$end = 254
+
+for ($i = $start; $i -le $end; $i++) {
+    $ip ="$networkprefix.$i"
+    try {
+    $result = Resolve-DnsName -Name $ip -Server $dnsserver -ErrorAction stop
+    if ($result) {
+        "$ip - " + $result.NameHost
+        }
+    } 
+    catch {
+    }
+
+ }
