9bf1d7f8aff29b6aeb76260290942ca3f9329f92
Andy <andy.phuong@mymail.champlain.edu>
2025-01-31 11:55:49 -0500

    week 3 dns

---
+++ week3/dnsres.ps1
@@ -0,0 +1,13 @@
+﻿param ($networkP, $Dserver)
+
+for ($i=0; $i -lt 255; $i++) {
+   
+    $name = Resolve-DnsName -DnsOnly $networkP'.'$i -Server $Dserver -ErrorAction Ignore | Select-Object -ExpandProperty "NameHost"
+    if ($null -ne $name) {
+        write-host $networkP'.'$i $name
+    } 
+  }
+    
+ 
+
+
